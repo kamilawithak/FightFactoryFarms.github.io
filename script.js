363d78b4a979a19715836be32dd6c3337358ebdf
kamilawithak <163339422+kamilawithak@users.noreply.github.com>
2024-03-25 17:08:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+script.js
+/* .js files add interaction to your website */
+var displayInfo = document.getElementById("message");
+var button = document.getElementById("submit");
+
+if (button) {
+  button.addEventListener("click", displayMessage);
+}
+
+function displayMessage() {
+  var name = document.getElementById("name").value
+  var city = document.getElementById("city").value
+  displayInfo.innerHTML = name + " you can make a difference in " + city + " by putting up flyers, attending meetings at anti-factory farm organizations or starting your own, and spreading awareness through word of mouth."
+}
+
